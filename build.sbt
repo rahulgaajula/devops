04b3c5601359adf467526c8967423e60068d0e53
rahul <rahul.gajula40@gmail.com>
2018-03-06 10:07:15 -0500

    first commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "hellodevops"
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+    
---
+++ src/main/scala/hellodevops.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by rgajula on 3/6/2018.
+  */
+class hellodevops {
+
+  def main(args: Array[String]): Unit = {
+
+    print("Hello world")
+
+  }
+}
